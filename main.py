5bd6345acea17c7a72b38e02e62d74340ed91016
HybridUofA <benjaminlukeedger@gmail.com>
2025-08-25 13:10:33 -0500

    implemented conversation storage, basic prompt input, and verbose flag

---
+++ main.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import sys
+
+def main():
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    if len(sys.argv) == 1:
+        print("Error! No prompt provided!")
+        sys.exit(1)
+    prompt = sys.argv[1]
+    messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,)
+    if "--verbose" in sys.argv:
+        print(f"User prompt: {prompt}")
+        print(response.text)
+        print(f'Prompt tokens: {response.usage_metadata.prompt_token_count}\nResponse tokens: {response.usage_metadata.candidates_token_count}')
+        sys.exit(0)
+    print(response.text)
+    sys.exit(0)
+if __name__ == "__main__":
+    main()

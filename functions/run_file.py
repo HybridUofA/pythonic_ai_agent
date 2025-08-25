83141a53d650969a456e3da3245835b5ec3c4387
HybridUofA <benjaminlukeedger@gmail.com>
2025-08-25 17:08:31 -0500

    added python writing function and useless system prompt for testing

---
+++ functions/run_file.py
@@ -0,0 +1,30 @@
+import os
+import subprocess
+
+def run_python_file(working_directory, file_path, args=[]):
+    target_path = os.path.join(working_directory, file_path)
+    abs_path = os.path.abspath(target_path)
+    if not abs_path.startswith(os.path.abspath(working_directory)):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(target_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    try:
+        result = subprocess.run(["python", file_path] + args, capture_output=True, timeout=30, text=True, cwd=working_directory)
+        output = []
+        if result.stdout.strip():
+            output.append(f"STDOUT:\n{result.stdout}")
+        if result.stderr.strip():
+            output.append(f"STDERR:\n{result.stderr}")
+        if result.returncode != 0:
+            output.append(f"Process exited with code {result.returncode}")
+        if output:
+            return "\n".join(output)
+        else:
+            return "No output produced."
+         
+                
+    except Exception as e:
+        return f'Error: executing Python file: {e}'
+    
--- main.py
+++ main.py
@@ -8,12 +8,13 @@ def main():
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
+    system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
     if len(sys.argv) == 1:
         print("Error! No prompt provided!")
         sys.exit(1)
     prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
-    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages,)
+    response = client.models.generate_content(model='gemini-2.0-flash-001', contents=messages, config=types.GenerateContentConfig(system_instruction=system_prompt),)
     if "--verbose" in sys.argv:
         print(f"User prompt: {prompt}")
         print(response.text)
